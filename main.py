c14036ad73e04fcb0029ddfa4a7e77e6c063e295
msaini0r <msaini0r@gmail.com>
2023-04-26 01:18:28 +0530

    basic image frame

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ import customtkinter as ctk
 from image_widgets import *
  
 class App(ctk.CTk):
-     def __init__(self):
+    def __init__(self):
+        #setup [frame size, name and size]
+        super().__init__()
+        ctk.set_appearance_mode('dark')
+        self.geometry('1000x600')
+        self.title('Photo Editor')
+        self.minsize(800,500)
          
-         #setup
-         super().__init__()
-         ctk.set_appearance_mode('dark')
-         self.geometry('1000x600')
-         self.title('Photo Editor')
-         self.minsize(800,500)
+        # layout
+        self.rowconfigure(0, weight = 1)
+        self.columnconfigure(0, weight = 2)
+        self.columnconfigure(1, weight = 6)
          
-         # layout
-         self.rowconfigure(0, weight = 1)
-         self.columnconfigure(0, weight = 2)
-         self.columnconfigure(1, weight = 6)
+        # widgets
          
+        # ImportButton (Frame with a button)
+        ImageImport(self, self.import_image)
          
-         # widgets
+        #run
+        self.mainloop()
          
-         # ImportButton (Frame with a button)
-         ImageImport(self)
-         
-         #run
-         self.mainloop()
+    def import_image(self, path):
+        print(path)
 
 App()
